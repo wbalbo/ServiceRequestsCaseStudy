9be056d9239899db0a1760f3bfb37c2abc9f92d8
Wellington Camargo <wellington_balbo_lp@hotmail.com>
2021-09-13 00:17:25 -0300

    - Creation of class to test api, needs more tests.

---
+++ Tests/ApiTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using ServiceRequests.Controllers;
+using ServiceRequests.Models;
+using ServiceRequests.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Tests
+{
+    public class ApiTests
+    {
+        private readonly MemoryCacheOptions opt = new MemoryCacheOptions();
+        private readonly IMemoryCache _memoryCache;
+
+        public ApiTests()
+        {
+            _memoryCache = new MemoryCache(opt);
+        }
+
+        [Fact]
+        public void GetAll_ShouldWork()
+        {
+            List<ServiceRequestModel> requests = new ServiceRequestRepository(_memoryCache).GetAll();
+            Assert.Equal(new List<ServiceRequestModel>(), requests);
+        }
+
+        [Fact]
+        public void GetById_ShouldWork()
+        {
+            Guid id = Guid.NewGuid();
+            ServiceRequestModel request = new ServiceRequestRepository(_memoryCache).GetById(id);
+            Assert.Null(request);
+        }
+    }
+}
