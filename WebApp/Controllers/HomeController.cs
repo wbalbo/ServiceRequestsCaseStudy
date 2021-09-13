d8780cd9db55b7eb5ac6dca64756acf721f3646c
Wellington Camargo <wellington_balbo_lp@hotmail.com>
2021-09-13 00:16:51 -0300

    - Improvements on WebApp project.

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using ServiceRequests.Models;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebApp.Controllers
@@ -18,10 +20,69 @@ namespace WebApp.Controllers
                 {
                     string apiResponse = await response.Content.ReadAsStringAsync();
                     serviceRequestsList = JsonConvert.DeserializeObject<List<ServiceRequestModel>>(apiResponse);
-                }   
+                }
             }
 
             return View(serviceRequestsList);
+        }
+
+        public ViewResult AddServiceRequest() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> AddServiceRequest(ServiceRequestModel model)
+        {
+            model = SetDefaultValues(model);
+
+            ServiceRequestModel modelFromApi = new ServiceRequestModel();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+ 
+                using (var response = await httpClient.PostAsync("http://localhost:33710/api/servicerequest", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    modelFromApi = JsonConvert.DeserializeObject<ServiceRequestModel>(apiResponse);
+                }
+            }
+            return View(modelFromApi);
+        }
+
+        public async Task<IActionResult> GetServiceRequest(Guid id)
+        {
+            ServiceRequestModel serviceRequestModel;
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:33710/api/servicerequest/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    serviceRequestModel = JsonConvert.DeserializeObject<ServiceRequestModel>(apiResponse);
+                }
+            }
+
+            return View(serviceRequestModel);
+        }
+
+        private ServiceRequestModel SetDefaultValues(ServiceRequestModel model)
+        {
+            model.Id = Guid.NewGuid();
+            model.CreatedDate = DateTime.Now;
+            model.Status = ServiceRequestModel.CurrentStatus.Created;
+
+            return model;
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteServiceRequest(int id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.DeleteAsync("https://localhost:33710/api/servicerequest/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
